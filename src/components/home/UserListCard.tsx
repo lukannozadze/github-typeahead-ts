d44a5e8882c53bb344f691ca974389e482cfa91c
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-03-02 21:56:02 +0400

    create user list card component

---
+++ src/components/home/UserListCard.tsx
@@ -0,0 +1,14 @@
+function UserListCard() {
+  return (
+    <ul className="flex flex-col gap-8">
+      <li>
+        <div className="px-4 py-4 max-w-[700px] min-w-[500px] flex items-center justify-between bg-[#1E2A47] rounded-md shadow-xl">
+          <h2 className="text-white font-bold text-xl font-mono">test</h2>
+          <img className="w-20 h-20 rounded-full" src="#" alt="#" />
+        </div>
+      </li>
+    </ul>
+  );
+}
+
+export default UserListCard;
